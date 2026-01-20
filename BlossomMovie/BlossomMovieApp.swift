6209aae517b8c7a8e0289fede6bfbc0310e6dbf9
alexcloudstar <alexcloudstar@gmail.com>
2026-01-20 19:38:03 +0200

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Stefan Alexandru Romila on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
